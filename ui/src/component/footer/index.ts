b5f60268036c4c521b93e6d9527988fb69e86cb3
alifprihantoro <alifprihantoro@gmail.com>
2023-11-28 12:37:37 +0700

    feat(footer): finish

---
+++ ui/src/component/footer/index.ts
@@ -0,0 +1,11 @@
+import { html } from '../../utils/html'
+
+export default html`
+  <footer
+    class="footer footer-center p-4 bg-base-300 text-base-content pb-14"
+    data-aos="fade-up">
+    <aside>
+      <p>Copyright © 2023 - All right reserved by Alief Prihantoro</p>
+    </aside>
+  </footer>
+`
