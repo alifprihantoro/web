0c1ce452a169c426f7a2d3c05698bd4e85934516
alifprihantoro <alifprihantoro@gmail.com>
2023-11-28 12:38:27 +0700

    feat(project): finish

---
+++ ui/src/component/project/listNvim.ts
@@ -0,0 +1,10 @@
+const LIST_NVIM = [
+  'muryp-git-setup.nvim',
+  'muryp-gh.nvim',
+  'muryp-link.nvim',
+  'muryp-checklist.nvim',
+  'nvim-muryp-snippet-collection',
+  'nvim-muryp-telescope-bookmark',
+]
+
+export default LIST_NVIM
---
+++ ui/src/component/project/listWeb.ts
@@ -0,0 +1,33 @@
+const LIST_WEB = [
+  {
+    title: 'web v1 from muryp',
+    repo: 'https://github.com/muryp/web',
+    web: '',
+  },
+  {
+    title: 'project api to learn from dicoding',
+    repo: 'https://github.com/alifprihantoro/dicoding-restaurant-app',
+    web: '',
+  },
+  {
+    title: 'web to promote serabi v1',
+    repo: 'https://github.com/alifprihantoro/serabi',
+    web: 'https://bicor.netlify.com',
+  },
+  {
+    title: 'project learn from freecode camp',
+    repo: 'https://github.com/alifprihantoro/freecode-camp',
+    web: '',
+  },
+  {
+    title: 'old theme muryp',
+    repo: 'https://github.com/alifprihantoro/muryp-new',
+    web: 'https://muryp.netlify.com',
+  },
+  {
+    title: 'project api to learn from dicoding',
+    repo: 'https://github.com/alifprihantoro/tugas-dicoding-bakend-api-book-app',
+  },
+]
+
+export default LIST_WEB
