247505493f11f0f3e2adec045ec68396f53901b3
alifprihantoro <alifprihantoro@gmail.com>
2024-05-04 04:53:47 +0700

    fix: rm dev css and change btn

--- ui/home/src/component/hero/component.ts
+++ ui/home/src/component/hero/component.ts
@@ -1,25 +1,23 @@
 import { html } from '../../utils/html'
 
-export interface Targs { IMG_URL: string, TITLE: string, DESCRIPTION: string }
+export interface Targs {
+  IMG_URL: string
+  TITLE: string
+  DESCRIPTION: string
+}
 const Component = ({ IMG_URL, TITLE, DESCRIPTION }: Targs) => {
   return html`
     <div class="hero min-h-screen ">
       <div class="hero-content flex-col sm:flex-row-reverse">
         <div class="avatar online" data-aos="fade-left">
           <div class="w-36 rounded-full">
-            <img
-              src="${IMG_URL}"
-              alt="profile"
-              fetch-priority="hight"
-            />
+            <img src="${IMG_URL}" alt="profile" fetch-priority="hight" />
           </div>
         </div>
         <div data-aos="fade-right">
           <h1 class="text-5xl font-bold">${TITLE}</h1>
-          <p class="py-6">
-            ${DESCRIPTION}
-          </p>
-          <button class="btn btn-primary">Hired Me!</button>
+          <p class="py-6">${DESCRIPTION}</p>
+          <a href="/" class="btn btn-primary">Hired Me!</a>
         </div>
       </div>
     </div>
