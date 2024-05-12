9c65e9f9b4d6b6511d061851305ec4c330af2b2e
alifprihantoro <alifprihantoro@gmail.com>
2024-05-13 06:10:55 +0700

    chore(configs): add url

--- configs/data/site.ts
+++ configs/data/site.ts
@@ -2,3 +2,4 @@ export const PROFILE_URL = '/profil.png'
 export const ICON_URL = '/profil.png'
 export const TITLE = 'ALIEF PRIHANTORO'
 export const DESCRIPTION = `Hello, I'm a web developer. my specific focus is development, tools and performance. I like simple web. I have some plugins on neovim.`
+export const PUBLIC_URL = 'https://alifprihantoro.github.io/public'
