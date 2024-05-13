a55eb0e81687e022bc3bdc954ac680da3d6fac8a
alifprihantoro <alifprihantoro@gmail.com>
2024-05-13 07:00:44 +0700

    fix: build astro

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config'
 
-const opts =
-  import.meta.env.PROD
-    ? {
-        outDir: '../dist',
-      }
-    : {
-        publicDir: '../public/',
-      }
+const opts = import.meta.env.PROD
+  ? {}
+  : {
+    publicDir: '../public/',
+  }
 // https://astro.build/config
 export default defineConfig(opts)
